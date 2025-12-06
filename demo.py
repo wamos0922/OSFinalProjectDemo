2e5de017cc64cf55205160f37b56e9ce86a4ec62
wamos0922 <wamos1910@hanyang.ac.kr>
2025-12-06 18:55:07 +0900

    Dummy Image Creator

--- demo.py
+++ demo.py
@@ -7,7 +7,12 @@ from PIL import Image
 
 
 # Create a Dummy Image.
-
+def create_dummy_image(filename="sample_input.png"):
+    """Creates a basic image if no input file exists for testing."""
+    if not os.path.exists(filename):
+        print(f"--- Creating a dummy image: {filename} ---")
+        img = Image.new('RGB', (300, 200), color='#6A5ACD')
+        img.save(filename)
 
 
 
